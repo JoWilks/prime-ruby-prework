10d812ecd6c69fda3aeae7fdeff3602d7af6abc7
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 18:58:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,11 @@
-def prime?(integer)
-not_these_no = [2,3,5,7,11]
-
-  if integer<2 
+def prime?(int)
+  if int <= 1
     false
-  elsif not_these_no.include?(integer)
+  elsif int <= 3
     true
-  elsif integer % 2 == 0
+  elsif int % 2 == 0 || int % 3 == 0
     false
   elsif (2..Math.sqrt(int)).none? {|i| (int % i).zero?}
-  else
     true
-    end
+  end
 end
