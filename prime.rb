04c828c43f3ffa25dbf6843a6b482316bd9494fb
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 19:14:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,9 @@
-def prime?(int)
-  if int <= 1
-    false
-  elsif int <= 3
-    true
-  elsif int % 2 == 0 || int % 3 == 0
-    false
-  elsif (2..Math.sqrt(int)).none? {|i| (int % i).zero?}
-    true
+def is_prime?(num)
+  if num <= 1
+    return nil
+  elsif (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
+    return true
+  else
+    return false
   end
 end
