ee565272a73dc581e140b93fa64828d40ad0c9bb
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 17:15:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,15 +4,16 @@ def prime?(int)
   
   if integer<2 
     false
-  elsif <=3
+  elsif int<=3
     true
-  elsif int % 2 == 0 || int mod 3 == 0
+  elsif int % 2 == 0 || int % 3 == 0
     false
   while i*i<=int
     if int % i == 0 || n % (i+2) == 0
       return false
     end
     i = i+6
-    return true
+    end
+  return true
   end
 end
