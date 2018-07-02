4d1178f498ba7800cf8fe8ca14c75986718fc053
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 15:50:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,12 @@
 def prime?(integer)
-  
-  i = 2
-   integer % i
-  
-  #all even numbers are divisible by 2
-  # known prime numbers 1,3,5,7,11,13,17,19,23
-  # for odd numbers, if is divisible by itself and 1, but not any number is a prime number.
-  #check 1, divisible by 2?
-  #check 2, divisible by 3,5,7,
-  #check number is not 1,3,5,7 if so check if remainder by 3,5,7 = 0, if so isn't prime number.
-  #9,11,13,17,19,23,29,31,37,39,41
+not_these_no = [1,2,3,5,7]
+div_by_these = [3,5,7]
+
+  if not_these_no.include?(integer)
+    true
+  elsif integer % 2 === 0
+    false
+  elsif div_by_these.each {|element| integer % element === 0}  
+    false
+  end
 end
